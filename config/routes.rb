d188b707d0d0394f61886b3eb773fba754b7bbe9
LoopFruits <Kensworldc@gmail.com>
2022-05-05 18:09:14 -0400

    added User model

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
   # root "articles#index"
   #testing configuration 
   get '/hello', to: 'application#hello_world'
-
+  post "/signup", to: "users#create"
+  get "/me", to: "users#show"
   get '*path',
       to: 'fallback#index',
       constraints: ->(req) { !req.xhr? && req.format.html? }
