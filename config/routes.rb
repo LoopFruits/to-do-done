bbe5845286976f58b076e4f87dbbe53474f75693
LoopFruits <Kensworldc@gmail.com>
2022-05-06 16:21:07 -0400

    added todos routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,10 @@
 Rails.application.routes.draw do
-  get 'todos/index'
-  get 'todos/create'
-  get 'todos/update'
-  get 'todos/destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
   #testing configuration 
+  resources :todos 
   get '/hello', to: 'application#hello_world'
   post "/signup", to: "users#create"
   get "/me", to: "users#show"
