17fef4fd195e4b9eb5d7b208bf7f5c8a35094db2
LoopFruits <Kensworldc@gmail.com>
2022-05-06 22:54:46 -0400

    seeding completed

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,13 +1,19 @@
 class TodosController < ApplicationController
+
+  #GET  /todos 
   def index
+    render json: Todos.all
   end
 
+  #POST
   def create
   end
 
+  #Put 
   def update
   end
 
+  #DELETE 
   def destroy
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+Todo.create(title: "Faker::WorldCup.team", done: false) 
+
+Todo.create(title: "Faker::WorldCup.team", done: true)
+
+puts "Seeding Complete"
