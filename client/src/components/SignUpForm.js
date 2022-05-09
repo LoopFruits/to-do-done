047ce261fe655277947accfac7b992a9893042d1
LoopFruits <Kensworldc@gmail.com>
2022-05-09 12:52:42 -0400

    signupform in progress

---
+++ client/src/components/SignUpForm.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+
+
+function SignUpForm({ onLogin }) {
+    const [firstName, setFirstname] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [passwordConfirmation, setPasswordConfirmation] = useState("");
+    const [errors, setErrors] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        setErrors([]);
+        setIsLoading(true);
+        fetch("/signup", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            first_name: firstName,
+            last_name: lastName,
+            username,
+            password,
+            password_confirmation: passwordConfirmation,
+          }),
+        }).then((r) => {
+          setIsLoading(false);
+          if (r.ok) {
+            r.json().then((user) => onLogin(user));
+          } else {
+            r.json().then((err) => setErrors(err.errors));
+          }
+        });
+      }
+
+}
