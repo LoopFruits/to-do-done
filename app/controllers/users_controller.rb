b3fc85a1d713b2d4645a877aa39b5ab06fc62100
LoopFruits <Kensworldc@gmail.com>
2022-05-09 12:08:25 -0400

    backend signup and auto login test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,34 @@
 class UsersController < ApplicationController
+
+    #Post /singup
     def create 
-        byebug
+        #saving users id in session hash 
+        user = User.create!(create_user_params)
+        session[:user_id] ||= user.id
+        #return json response with info stored in create_user_params, and HTTP status code of 201
+        render json: user, status: :created
+        #if user is not valid, retuns JSON resp with error msg 422
+    rescue ActiveRecord::RecordInvalid => invalid 
+        render json: { errors: [invalid.record.errors] }, status: :unprocessable_entity
     end
 
+    #GET  
+    def show
+        user_id = session[:user_id]
+        #if user is logged return json sesp with user info and https satus 201 (created)
+        if user_id
+            user = User.find(user_id)
+            render json: user, status: :created
+        #if not logged in return json resp of 401(unauthorized)
+        else
+            render json: { error: "Unauthorized" }, status: :unauthorized
+        end
+    end
 
+    private 
+
+    def create_user_params 
+        params.permit(:first_name, :last_name, :username, :password )
+    end
 
 end
--- db/migrate/20220507013334_create_todos.rb
+++ db/migrate/20220507013334_create_todos.rb
@@ -1,6 +1,7 @@
 class CreateTodos < ActiveRecord::Migration[7.0]
   def change
     create_table :todos do |t|
+      t.belongs_to :user, null: false, foreign_key: true
       t.string :title
       t.boolean :done
       
