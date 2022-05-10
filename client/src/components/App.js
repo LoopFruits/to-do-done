fc721ad50b0d3ea5b9d97a6861d0b134d4d0758d
LoopFruits <Kensworldc@gmail.com>
2022-05-10 11:17:25 -0400

    Loginform error fixed

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,15 +1,20 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import SignUpForm from './SignUpForm'
+import Login from "../pages/Login";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
-    fetch("/hello")
-      .then((r) => r.json())
-      .then((data) => setCount(data.count));
+    // auto-login
+    fetch("/me").then((r) => {
+      if (r.ok) {
+        r.json().then((user) => setUser(user));
+      }
+    });
   }, []);
+
+  if (!user) return <Login onLogin={setUser} />;
 
   return (
     <BrowserRouter>
@@ -19,7 +24,6 @@ function App() {
             <h1>Test Route</h1>
           </Route>
           <Route path="/">
-            <h1>Page Count: {count}</h1>
           </Route>
         </Switch>
       </div>
--- client/src/components/SignUpForm.js
+++ client/src/components/SignUpForm.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
+import { Button, Error, Input, FormField, Label, Textarea } from "../styles";
 
 
 function SignUpForm({ onLogin }) {
-    const [firstName, setFirstname] = useState("");
+    const [firstName, setFirstName] = useState("");
     const [lastName, setLastName] = useState("");
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
@@ -34,8 +35,69 @@ function SignUpForm({ onLogin }) {
             r.json().then((err) => setErrors(err.errors));
           }
         });
-      }
+    }
 
+    return (
+        <form onSubmit={handleSubmit}>
+          <FormField>
+            <Label htmlFor="username">Username</Label>
+            <Input
+              type="text"
+              id="username"
+              autoComplete="off"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </FormField>
+          <FormField>
+            <Label htmlFor="password">Password</Label>
+            <Input
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              autoComplete="current-password"
+            />
+          </FormField>
+          <FormField>
+            <Label htmlFor="password">Password Confirmation</Label>
+            <Input
+              type="password"
+              id="password_confirmation"
+              value={passwordConfirmation}
+              onChange={(e) => setPasswordConfirmation(e.target.value)}
+              autoComplete="current-password"
+            />
+          </FormField>
+          <FormField>
+            <Label htmlFor="firstName">First Name</Label>
+            <Input
+              type="text"
+              id="first_name"
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+            />
+          </FormField>
+          <FormField>
+            <Label htmlFor="lastName">Last Name</Label>
+            <Textarea
+              rows="3"
+              id="last_name"
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
+            />
+          </FormField>
+          <FormField>
+            <Button type="submit">{isLoading ? "Loading..." : "Sign Up"}</Button>
+          </FormField>
+          <FormField>
+            {errors.map((err) => (
+              <Error key={err}>{err}</Error>
+            ))}
+          </FormField>
+        </form>
+    );
+    
 
 }
 
