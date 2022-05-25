bd467759c25bd772cef8f907f121fedb99b2f3e7
LoopFruits <Kensworldc@gmail.com>
2022-05-25 15:32:28 -0400

    features work

--- client/src/styles/Button.js
+++ client/src/styles/Button.js
@@ -2,12 +2,12 @@ import styled from "styled-components";
 
 const COLORS = {
   primary: {
-    "--main": "indigo",
+    "--main": "#4287f5",
     "--accent": "white",
   },
   secondary: {
     "--main": "lavenderblush",
-    "--accent": "indigo",
+    "--accent": "#4287f5",
   },
 };
 
